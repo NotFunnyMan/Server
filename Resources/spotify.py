7f9ae1d1d4e8ffed98fb7345142f40797457d24c
Alexey Cheldaev <alexothers35@gmail.com>
2019-02-17 17:23:04 +0700

    Добавлено логгирование
    Signed-off-by: Alexey Cheldaev <alexothers35@gmail.com>

--- Resources/spotify.py
+++ Resources/spotify.py
@@ -3,6 +3,14 @@ import spotipy.util as util
 import os
 import time
 import json
+import logging
+
+logger = logging.getLogger("Spotify")
+logger.setLevel(logging.DEBUG)
+fh = logging.FileHandler("server.log")
+formatter = logging.Formatter(u"%(asctime)s : %(levelname)-5s : %(filename)s : %(name)s logger : %(message)s")
+fh.setFormatter(formatter)
+logger.addHandler(fh)
 
 
 class Artist(object):
@@ -50,7 +58,7 @@ class Artist(object):
         :return:
             Последний альбом по каждой из категории
         """
-
+        logger.info('Getting a latest compositions for artist: %s...' % self.name)
         return self.get_albums(limit=1)[0], self.get_singles_or_eps(limit=1)[0], \
                self.get_appears_on(limit=1)[0], self.get_compilations(limit=1)[0]
 
@@ -63,18 +71,21 @@ class Artist(object):
         :return:
             Список альбомов
         """
-
-        albums = self.connector.artist_albums(self.artist_id, album_type='album', limit=limit)
-        artist_albums = albums_list_from_json(albums['items'])
-        if limit is None:
-            limit = albums['total']
-        while albums['next']:
-            if len(artist_albums) < limit:
-                albums = self.connector.next(albums)
-                artist_albums.append(albums['items'])
-            elif len(artist_albums) >= limit:
-                return artist_albums[:limit]
-
+        try:
+            logger.info('Getting %s albums' % str(limit) if limit is not None else 'all')
+            albums = self.connector.artist_albums(self.artist_id, album_type='album', limit=limit)
+            artist_albums = albums_list_from_json(albums['items'])
+            if limit is None:
+                limit = albums['total']
+            while albums['next']:
+                if len(artist_albums) < limit:
+                    albums = self.connector.next(albums)
+                    artist_albums.append(albums['items'])
+                elif len(artist_albums) >= limit:
+                    return artist_albums[:limit]
+        except Exception as e:
+            logger.error('Cannot get the albums!\n%s' % e)
+            return [Album('', '', '', '', '', '', '', '')]
         return [Album('', '', '', '', '', '', '', '')] if len(artist_albums) == 0 else artist_albums
 
     def get_singles_or_eps(self, limit=None):
@@ -86,17 +97,21 @@ class Artist(object):
         :return:
             Список синглов или EP
         """
-        singles_eps = self.connector.artist_albums(self.artist_id, album_type='single')
-        artist_singles = albums_list_from_json(singles_eps['items'])
-        if limit is None:
-            limit = singles_eps['total']
-        while singles_eps['next']:
-            if len(artist_singles) < limit:
-                singles_eps = self.connector.next(singles_eps)
-                artist_singles.append(singles_eps['items'])
-            elif len(artist_singles) >= limit:
-                return artist_singles[:limit]
-
+        try:
+            logger.info('Getting %s singles or EPs' % str(limit) if limit is not None else 'all')
+            singles_eps = self.connector.artist_albums(self.artist_id, album_type='single')
+            artist_singles = albums_list_from_json(singles_eps['items'])
+            if limit is None:
+                limit = singles_eps['total']
+            while singles_eps['next']:
+                if len(artist_singles) < limit:
+                    singles_eps = self.connector.next(singles_eps)
+                    artist_singles.append(singles_eps['items'])
+                elif len(artist_singles) >= limit:
+                    return artist_singles[:limit]
+        except Exception as e:
+            logger.error('Cannot get the single or EP!\n%s' % e)
+            return [Album('', '', '', '', '', '', '', '')]
         return [Album('', '', '', '', '', '', '', '')] if len(artist_singles) == 0 else artist_singles
 
     def get_appears_on(self, limit=None):
@@ -108,17 +123,21 @@ class Artist(object):
         :return:
             Список произведений
         """
-        appears = self.connector.artist_albums(self.artist_id, album_type='appears_on')
-        artist_appears = albums_list_from_json(appears['items'])
-        if limit is None:
-            limit = appears['total']
-        while appears['next']:
-            if len(artist_appears) < limit:
-                appears = self.connector.next(appears)
-                artist_appears.append(appears['items'])
-            elif len(artist_appears) >= limit:
-                return artist_appears[:limit]
-
+        try:
+            logger.info('Getting %s appears on' % str(limit) if limit is not None else 'all')
+            appears = self.connector.artist_albums(self.artist_id, album_type='appears_on')
+            artist_appears = albums_list_from_json(appears['items'])
+            if limit is None:
+                limit = appears['total']
+            while appears['next']:
+                if len(artist_appears) < limit:
+                    appears = self.connector.next(appears)
+                    artist_appears.append(appears['items'])
+                elif len(artist_appears) >= limit:
+                    return artist_appears[:limit]
+        except Exception as e:
+            logger.error('Cannot get the appears on!\n%s' % e)
+            return [Album('', '', '', '', '', '', '', '')]
         return [Album('', '', '', '', '', '', '', '')] if len(artist_appears) == 0 else artist_appears
 
     def get_compilations(self, limit=None):
@@ -130,17 +149,21 @@ class Artist(object):
         :return:
             Список подборок
         """
-        compilations = self.connector.artist_albums(self.artist_id, album_type='compilation')
-        artist_compilations = albums_list_from_json(compilations['items'])
-        if limit is None:
-            limit = compilations['total']
-        while compilations['next']:
-            if len(artist_compilations) < limit:
-                compilations = self.connector.next(compilations)
-                artist_compilations.append(compilations['items'])
-            elif len(artist_compilations) >= limit:
-                return artist_compilations[:limit]
-
+        try:
+            logger.info('Getting %s compilations' % str(limit) if limit is not None else 'all')
+            compilations = self.connector.artist_albums(self.artist_id, album_type='compilation')
+            artist_compilations = albums_list_from_json(compilations['items'])
+            if limit is None:
+                limit = compilations['total']
+            while compilations['next']:
+                if len(artist_compilations) < limit:
+                    compilations = self.connector.next(compilations)
+                    artist_compilations.append(compilations['items'])
+                elif len(artist_compilations) >= limit:
+                    return artist_compilations[:limit]
+        except Exception as e:
+            logger.error('Cannot get the compilation!\n%s' % e)
+            return [Album('', '', '', '', '', '', '', '')]
         return [Album('', '', '', '', '', '', '', '')] if len(artist_compilations) == 0 else artist_compilations
 
 
@@ -226,80 +249,86 @@ def albums_list_from_json(js):
     return [album_from_json(part) for part in js]
 
 
-def check_updates(init_art_list, curr_art_list, DELAY_API_REQUEST):
+def check_updates(init_art_list, curr_art_list, delay_api_request):
     updates = []
+    logger.info('Checking any updates...')
     for artist in curr_art_list:
         if artist in init_art_list:
             album, single_ep, appears, compilation = artist.get_latest_compositions()
             init_artist = init_art_list[init_art_list.index(artist)]
             if album != init_artist.latest_album:
-                print('NEW ALBUM!\n')
+                logger.info('New album!')
                 last_10_albums = artist.get_albums(limit=10)
                 new_ones = last_10_albums[:last_10_albums.index(init_artist.latest_album)]
                 updates.extend(new_ones)
-                print('Artist:%s\nNew albums\n' % artist.name)
+                logger.info('Artist:%s\nNew albums\n' % artist.name)
                 for ones in new_ones:
-                    print('Name: %s' % ones.name)
+                    logger.info('Name: %s' % ones.name)
                 artist.latest_album = album
             if single_ep != init_artist.latest_single:
-                print('NEW SINGLE OR EP!\n')
+                logger.info('New sing or EP!')
                 last_10_single = artist.get_singles_or_eps(limit=10)
                 new_ones = last_10_single[:last_10_single.index(init_artist.latest_single)]
                 updates.extend(new_ones)
-                print('Artist:%s\nNew single or EP\n' % artist.name)
+                logger.info('Artist:%s\nNew single or EP\n' % artist.name)
                 for ones in new_ones:
-                    print('Name: %s' % ones.name)
+                    logger.info('Name: %s' % ones.name)
                 artist.latest_single = single_ep
             if appears != init_artist.latest_appears:
-                print('NEW APPEARS!\n')
+                logger.info('New appears!\n')
                 last_10_appears = artist.get_albums(limit=10)
                 new_ones = last_10_appears[:last_10_appears.index(init_artist.latest_appears)]
                 updates.extend(new_ones)
-                print('Artist:%s\nNew appears\n' % artist.name)
+                logger.info('Artist:%s\nNew appears\n' % artist.name)
                 for ones in new_ones:
-                    print('Name: %s' % ones.name)
+                    logger.info('Name: %s' % ones.name)
                 artist.latest_appears = appears
             if compilation != init_artist.latest_compilation:
-                print('NEW COMPILATION!\n')
+                logger.info('New compilation!\n')
                 last_10_compilations = artist.get_compilations(limit=10)
                 new_ones = last_10_compilations[:last_10_compilations.index(init_artist.latest_compilation)]
                 updates.extend(new_ones)
-                print('Artist:%s\nNew albums\n' % artist.name)
+                logger.info('Artist:%s\nNew albums\n' % artist.name)
                 for ones in new_ones:
-                    print('Name: %s' % ones.name)
+                    logger.info('Name: %s' % ones.name)
                 artist.latest_compilation = compilation
-            time.sleep(DELAY_API_REQUEST)
-            print('TIME TO SLEEP....\n\n\n')
+            logger.info('Time to sleep....')
+            time.sleep(delay_api_request)
     return updates
 
 
 def get_token(conf):
     token = None
+    logger.info('Creating a token...')
     try:
         token = util.prompt_for_user_token(conf['username'],
                                            scope=conf['SPOTIPY_SCOPE'],
                                            client_id=conf['SPOTIPY_CLIENT_ID'],
                                            client_secret=conf['SPOTIPY_CLIENT_SECRET'],
                                            redirect_uri=conf['SPOTIPY_REDIRECT_URI'])
-    except:
+    except Exception as e:
+        logger.error('Cannot get a token! Trying to remove cache-file and try again!\n%s' % e)
         os.remove(f".cache-{conf['username']}")
         token = util.prompt_for_user_token(conf['username'],
                                            scope=conf['SPOTIPY_SCOPE'],
                                            client_id=conf['SPOTIPY_CLIENT_ID'],
                                            client_secret=conf['SPOTIPY_CLIENT_SECRET'],
                                            redirect_uri=conf['SPOTIPY_REDIRECT_URI'])
-    finally:
-        return token
+    return token
 
 
 def get_following_artists(spotify):
-    # Get all artists which I follow
-    result = spotify.current_user_followed_artists()
-    art_list = artists_list_from_json(result['artists']['items'], spotify)
-    while result['artists']['next']:
-        result = spotify.next(result['artists'])
-        art_list.extend(artists_list_from_json(result['artists']['items'], spotify))
-    return art_list
+    logger.info('Getting follow artists...')
+    try:
+        result = spotify.current_user_followed_artists()
+        art_list = artists_list_from_json(result['artists']['items'], spotify)
+        while result['artists']['next']:
+            result = spotify.next(result['artists'])
+            art_list.extend(artists_list_from_json(result['artists']['items'], spotify))
+        return art_list
+    except Exception as e:
+        logger.error('Cannot get following artists!\n%s' % e)
+        return []
 
 
 def get_artists_list(conf):
@@ -309,11 +338,17 @@ def get_artists_list(conf):
 
 
 def get_conf():
-    with open('../spotify_conf.json') as f:
-        return json.load(f)
+    logger.info('Reading a json configuration file...')
+    try:
+        with open('../spotify_conf.json') as f:
+            return json.load(f)
+    except Exception as e:
+        logger.error('Cannot read json config!\n%s' % e)
+        return {}
 
 
 def main():
+    logger.info('Spotify process was started...')
     conf = get_conf()
     init_artists_list = get_artists_list(conf)
 
@@ -322,12 +357,12 @@ def main():
             current_artists_list = get_artists_list(conf)
             updates = check_updates(init_artists_list, current_artists_list, conf['DELAY_API_REQUEST'])
             if updates:
-                pass
+                logger.info('We got any updates!')
             init_artists_list = current_artists_list
-            print('SLEEP DELAY_BTW_GET_UPDATES')
+            logger.info('Sleep DELAY_BTW_GET_UPDATES...')
             time.sleep(conf['DELAY_BTW_GET_UPDATES'])
         except Exception as e:
-            print('EXCEPTION!\n%s' % e)
+            logger.error('Exception in main loop!\n%s' % e)
 
 
 if __name__ == '__main__':
